1a892053f4ea3cf876ba5d277c2653d37d8c73f6
gukazioka <gugakazioka@gmail.com>
2026-01-10 20:17:54 -0300

    feat: get table from url

--- src/fetcher/fetcher.py
+++ src/fetcher/fetcher.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-
 class WebFetcher:
 
     def __init__(
@@ -12,16 +10,18 @@ class WebFetcher:
         url : str
     ):
         options = webdriver.ChromeOptions()
+        # TODO: Adicionar options necessárias para melhorar o desempenho
+        options.page_load_strategy = 'eager'
         # options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=options)
         self.wait = WebDriverWait(self.driver, timeout=2)
         self.driver.get(url)
-        self.fetch("China")
 
-    def fetch(self, region : str):
+    def get_table(self, region : str) -> str:
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="add-filters-button"]'))).click()
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="filter-category-market_data"]'))).click()
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="filter-metric-region"]'))).click()
+        # TODO: Verificar se mais algum está selecionado.
         eua_input = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, f'label[title="United States"] input')))
         if eua_input.is_selected():
             eua_input.click()
@@ -29,3 +29,4 @@ class WebFetcher:
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="filter-apply"]'))).click()
         table_div = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="screener-table"]')
         table = table_div.find_element(By.CSS_SELECTOR, "table")
+        return table.get_attribute('outerHTML')

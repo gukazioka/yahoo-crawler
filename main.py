6c93bae483300dc4f85b9dd1acd21e369f051fcb
gukazioka <gugakazioka@gmail.com>
2026-01-11 17:43:10 -0300

    feat: add cli interface and remove comments

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.service.crawler import Crawler
 from src.storage.csv import CSVWriter
+import sys
 
 if __name__ == '__main__':
-    crawler = Crawler('https://finance.yahoo.com/research-hub/screener/equity/', CSVWriter)
-    crawler.get_equities('China')
+    try:
+        region = sys.argv[1]
+        crawler = Crawler('https://finance.yahoo.com/research-hub/screener/equity/', CSVWriter)
+        crawler.get_equities(region)
+    except IndexError:
+        print('Region must be informed.')
--- src/fetcher/fetcher.py
+++ src/fetcher/fetcher.py
@@ -11,8 +11,8 @@ class WebFetcher:
         url : str
     ):
         options = webdriver.ChromeOptions()
-        # TODO: Adicionar options necessárias para melhorar o desempenho
         options.page_load_strategy = 'eager'
+        options.add_argument("--window-size=1920,1080")
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
@@ -22,19 +22,17 @@ class WebFetcher:
         self.wait = WebDriverWait(self.driver, timeout=5, poll_frequency=0.1)
         self.driver.get(url)
 
-    # ElementClickInterceptedException
     def set_region(self, region : str):
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="add-filters-button"]'))).click()
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="filter-category-market_data"]'))).click()
-        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="filter-metric-region"]'))).click()
-        # TODO: Verificar se mais algum está selecionado.
-        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, f'label[title="United States"] input')))
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="filter-metric-region"]'))).click()
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, f'label[title="United States"] input')))
         eua_input = self.driver.find_element(By.CSS_SELECTOR, f'label[title="United States"] input')
         if eua_input.is_selected():
             eua_input.click()
         self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, f'label[title="{region}"] input'))).click()
         updated = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="table-cell-ticker"]').get_attribute('innerHTML')
-        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="filter-apply"]'))).click()
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="filter-apply"]'))).click()
         self.wait.until(lambda e : e.find_element(By.CSS_SELECTOR, '[data-testid="table-cell-ticker"]').get_attribute('innerHTML') != updated)
 
     def get_table(self):
@@ -45,9 +43,6 @@ class WebFetcher:
         while next_button.is_enabled():
             updated = table.find_element(By.CSS_SELECTOR, '[data-testid="table-cell-ticker"]').get_attribute('innerHTML')
             next_button.click()
-            start = time.perf_counter()
             self.wait.until(lambda e : e.find_element(By.CSS_SELECTOR, '[data-testid="table-cell-ticker"]').get_attribute('innerHTML') != updated)
-            end = time.perf_counter()
-            print(f'demorou {end - start:.8f}')
             table = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="screener-table"]')
             yield table.get_attribute('innerHTML')

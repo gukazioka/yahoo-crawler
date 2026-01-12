e261bb05dcc6a5f3bf29a5c282dac6ef0a300002
gukazioka <gugakazioka@gmail.com>
2026-01-12 10:41:35 -0300

    feat: improve main

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import sys
 import os
 
 if __name__ == '__main__':
-    try:
-        if os.path.exists('equities.csv'):
-            os.remove('equities.csv')
+    if os.path.exists('equities.csv'):
+        os.remove('equities.csv')
 
-        region = sys.argv[1]
-        crawler = Crawler('https://finance.yahoo.com/research-hub/screener/equity/', CSVWriter)
-        crawler.get_equities(region)
-    except IndexError:
+    if len(sys.argv) < 2:
         print('Region must be informed.')
+        sys.exit(1)
+
+    region = sys.argv[1]
+    crawler = Crawler('https://finance.yahoo.com/research-hub/screener/equity/', CSVWriter)
+    crawler.get_equities(region)

dcfe1ff7dda2ec510ff85d5f7640cf1acde30aaf
gukazioka <gugakazioka@gmail.com>
2026-01-12 08:43:27 -0300

    feat: use headless mode

--- src/fetcher/fetcher.py
+++ src/fetcher/fetcher.py
@@ -12,7 +12,9 @@ class WebFetcher:
     ):
         options = webdriver.ChromeOptions()
         options.page_load_strategy = 'eager'
+        options.add_argument("--headless=new")
         options.add_argument("--window-size=1920,1080")
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
@@ -20,14 +22,14 @@ class WebFetcher:
         options.add_experimental_option("prefs", prefs)
 
         self.driver = webdriver.Chrome(options=options)
-        self.wait = WebDriverWait(self.driver, timeout=5, poll_frequency=0.1)
+        self.wait = WebDriverWait(self.driver, timeout=8, poll_frequency=0.1)
         self.driver.get(url)
 
     def set_region(self, region : str):
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="add-filters-button"]'))).click()
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="filter-category-market_data"]'))).click()
-        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="filter-metric-region"]'))).click()
-        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, f'label[title="United States"] input')))
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="filter-metric-region"]'))).click()
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, f'label[title="United States"] input')))
 
         eua_input = self.driver.find_element(By.CSS_SELECTOR, f'label[title="United States"] input')
         if eua_input.is_selected():
@@ -36,9 +38,9 @@ class WebFetcher:
         if not self.driver.find_elements(By.CSS_SELECTOR, f'label[title="{region}"] input'):
             raise InvalidRegion('Invalid region.')
 
-        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, f'label[title="{region}"] input'))).click()
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, f'label[title="{region}"] input'))).click()
         updated = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="table-cell-ticker"]').get_attribute('innerHTML')
-        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="filter-apply"]'))).click()
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="filter-apply"]'))).click()
         self.wait.until(lambda e : e.find_element(By.CSS_SELECTOR, '[data-testid="table-cell-ticker"]').get_attribute('innerHTML') != updated)
 
     def get_table(self):
